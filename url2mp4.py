ed526c20ec16722a06e9a9772405a8d443bbb1cf
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-02-15 23:05:43 +0900

    fix url2mp4.py
    Standard output is now output with python print statements.

--- url2mp4.py
+++ url2mp4.py
@@ -2,5 +2,14 @@ import subprocess
 
 ytURL = "URL"
 
-result = subprocess.run(['yt-dlp', '-f', 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]', ytURL])
-print(result)
+process = subprocess.Popen(['yt-dlp', '-f', 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]', ytURL], \
+                           shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+while True:
+    output = process.stdout.readline()
+    if output == '' and process.poll() is not None:
+        break
+    if output:
+        print(output.strip(), "\n")
+
+input("Press Enter to finish.")
